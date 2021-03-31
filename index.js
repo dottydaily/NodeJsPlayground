8bbb380339386394d7a415758fd31148f4bb8827
Pornpat Santibuppakul <dottypurkt@gmail.com>
2021-03-31 18:01:35 +0700

    Implement GET/POST/PUT/DELETE method

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World! from GET method.')
+})
+
+app.post('/', (req, res) => {
+    res.send('Hello World! from POST method.')
+})
+
+app.put('/user', (req, res) => {
+    res.send('Got a PUT request at /user.')
+})
+
+app.delete('/user', (req, res) => {
+    res.send('Got a DELETE request at /user')
+})
+
+// Serve files in directory name 'images'
+app.use('/files', express.static('public'))
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
