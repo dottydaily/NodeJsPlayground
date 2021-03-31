03daa7e7b64e1f2f6fa83a38649e4e3b7a750fe7
Pornpat Santibuppakul <dottypurkt@gmail.com>
2021-03-31 15:38:41 +0700

    Add some example

---
+++ 7-event/demo_event.js
@@ -0,0 +1,6 @@
+var fs = require('fs');
+var rs = fs.createReadStream('./demofile.txt');
+
+rs.on('open', () => {
+    console.log('The file is open.');
+});
---
+++ 7-event/demo_eventemitter.js
@@ -0,0 +1,13 @@
+var event = require('events');
+var eventEmitter = new event.EventEmitter();
+
+// Create an event handler
+var myEventHandler = () => {
+    console.log('I hear a scream!');
+}
+
+// Assign the event handler to an event
+eventEmitter.on('scream', myEventHandler);
+
+// Fire the 'scream' event
+eventEmitter.emit('scream');
